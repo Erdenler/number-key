384e5bd2d66ba69255111ecdbcfcc04a442819cf
Erdenler <108900630+Erdenler@users.noreply.github.com>
2023-01-29 17:08:18 +0300

    number key
    telephone directory app

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Contacts from "./compenents/Contacts"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Contacts />
+
+    </div>
+  );
+}
+
+export default App;
